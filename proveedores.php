3c94f5485984b4296fb1ad4ce78df3f37abf4224
monsebolena <monsebaldo7@gmail.com>
2025-01-28 19:59:03 +0100

    nueva direccion

--- proveedores.php
+++ proveedores.php
@@ -23,7 +23,7 @@ $result = $conexion->query($sql);
                     echo "<td>" . $fila['id'] . "</td>";
                     echo "<td>" . $fila['nombre'] . "</td>";
                     echo "<td>" . $fila['web'] . "</td>";
-                    echo "<td><a href='editar_proveedor.php?id=" . $fila['id'] . "'>Editar</a> | <a href='eliminar_proveedor.php?id=" . $fila['id'] . "'>Eliminar</a></td>";
+                    echo "<td><a href='editar_proveedor.php?id=" . $fila['id'] . "'>Editar</a> | <a href='eliminar_proveedor.php?id=" . $fila['id'] . "'>Eliminar</a> | <a href='proveedores?id=" . $fila['id'] . "'>Añadir</a></td>";
                     echo "</tr>";
                 }
                 ?>
@@ -44,6 +44,39 @@ $result = $conexion->query($sql);
                 <input type="submit" value="Guardar" class="btn btn-primary">
             </div>
         </form>
+        <hr>
+        <?php if(isset($_GET["id"])){ ?>
+        <div class="contacto">
+            <div>
+                <h3>Nueva dirección</h3>
+                <form action="nueva_direccion.php" method="post">
+                    <input type="hidden" name="idproveedor" value="<?php echo $_GET["id"] ?>">
+                    <label for="calle">calle</label>
+                    <input type="text" name="calle" id="calle" required placeholder="Calle">
+                    <label for="numero">Número</label>
+                    <input type="text" name="numero" id="numero" required placeholder="Número"> 
+                    <label for="comuna">Comuna</label>
+                    <input type="text" name="comuna" id="comuna" required placeholder="Comuna">
+                    <label for="ciudad">Ciudad</label>
+                    <input type="text" name="ciudad" id="ciudad" required placeholder="Ciudad">
+                    <input type="submit" value="Guardar">
+                    <input type="reset" value="Cancelar">
+                </form>
+            </div>
+            <div>
+                <h3>Nuevo teléfono</h3>
+                <form action="nuevo_telefono.php" method="post">
+                    <input type="hidden" name="idproveedor" value="<?php echo $_GET["id"] ?>">
+                    <label for="numero">Teléfono</label>
+                    <input type="text" name="numero" id="numero" required placeholder="Teléfono">
+                    <input type="submit" value="Guardar">
+                    <input type="reset" value="Cancelar">
+                </form>
+            </div>
+
+        </div>
+        <a href="proveedores">Volver</a>
+        <?php } ?>
       
     </div>
 
